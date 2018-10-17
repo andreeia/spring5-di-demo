8127edccbf232e24a6eb8768e4cbe0d257e25c15
andreeia <andreea.popescu_acz@yahoo.com>
2018-10-17 14:45:43 +0300

    using-qualifier:@Qualifier+propertyName

--- src/main/java/guru/springframework/didemo/controllers/GetterInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/GetterInjectedController.java
@@ -1,21 +1,22 @@
 package guru.springframework.didemo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import guru.springframework.didemo.services.GreetingService;
-import guru.springframework.didemo.services.GreetingServiceImpl;
 
 @Controller
-public class SetterInjectedController {
+public class GetterInjectedController {
 
 	private GreetingService greetingService;
 	
 	public String sayHello() {
 		return greetingService.sayGreeting();
 	}
+	
 	@Autowired
-	public void setGreetingService(GreetingServiceImpl greetingService) {
+	public void setGreetingService(@Qualifier("getterGreetingService")GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
 }
--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -3,17 +3,17 @@ package guru.springframework.didemo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import guru.springframework.didemo.services.GreetingServiceImpl;
+import guru.springframework.didemo.services.GreetingService;
 
 @Controller
 public class PropertyInjectedController {
 	//public GreetingServiceImplementation
 	//example of what NOT TO DO!!
 	@Autowired
-	public GreetingServiceImpl greetingService;
+	public GreetingService greetingServiceImpl;
 	
 	public String sayHello() {
-		return greetingService.sayGreeting();
+		return greetingServiceImpl.sayGreeting();
 	}
 	
 }
